3d4daabeeda4a12544abd79dc9bc766bd9b080eb
Tomasz Dłuski <mr.toumash@gmail.com>
2020-08-19 17:14:31 +0200

    feat: storybook

---
+++ src/components/Promotion.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Promotion = ({title, bank, image}) => {
+    return (
+        <div>
+            <h1>{title}</h1>
+            Component!
+        </div>
+    );
+};
+
+export default Promotion;
